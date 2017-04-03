1e1c4895dff9a9b601b54bfeacd945457489be7a
murr_aelaa <aelaau@gmail.com>
2017-04-03 23:30:17 +0400

    Demo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,41 @@
 module ApplicationHelper
   include Auth
+
+  def favorites
+    [[
+      '1428137018_zooshop_09.jpg',
+      'tumblr_lhljggyXEc1qgnva2o1_500.jpg',
+      'tumblr_lxwh6ui0Ae1qd8rbvo1_500.jpg',
+      'tumblr_m1ofb4iwjh1qzo3c9o1_500.jpg',
+      'tumblr_m3vhpplOD81qejbiro1_500.jpg'
+    ],
+    [
+      '7eqdso1IXor1epvxCnA1OvXso1_400.jpg',
+      'tumblr_lhoey7P1lb1qfyzelo1_500.jpg',
+      'tumblr_lxy2hu4Mfi1qbe5pxo1_500.jpg',
+      'tumblr_m1rewaGy2S1qa4yh0o1_500.png',
+      'tumblr_m3xustlc3p1qgkai1o1_500.jpg'
+    ],
+    [
+      'A2BVSY3o8cmyyjazPY8LX5Ew_500.jpg',
+      'tumblr_lhufvzh0d91qfyzelo1_500.jpg',
+      'tumblr_lxyl6zl2OK1qa9omho1_500.jpg',
+      'tumblr_m1ujd2wvTx1r6b7kmo1_500.jpg',
+      'tumblr_m3yr72DT8e1r6b7kmo1_500.jpg'
+    ],
+    [
+      'CvMiQoCWgAAaQpn.jpg',
+      'tumblr_li37rgQ8p01qgnva2o1_500.jpg',
+      'tumblr_lybh2miFC01qe4m80o1_500.jpg',
+      'tumblr_m1viu1ds121r81frto1_500.png',
+      'tumblr_m42qd2tdbc1qzex9io1_500.jpg'
+    ],
+    [
+      'Cw0Gm7-XEAAbwQ9.jpg',
+      'tumblr_li6itvRslB1qfyzelo1_500.jpg',
+      'tumblr_lyyhudmUmU1qgjltdo1_500.jpg',
+      'tumblr_m1vuajvY5r1qze0hyo1_500.jpg',
+      'tumblr_m435w24Wtr1qzuoq3o1_500.jpg'
+    ]]
+  end
 end
